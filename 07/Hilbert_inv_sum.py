11be4ca6e446628da53537bdf18a0ff8c59cd68c
Jonáš Venc <91418435+JonasVenc@users.noreply.github.com>
2023-12-06 13:24:58 +0100

    Update Hilbert_inv_sum.py

--- 07/Hilbert_inv_sum.py
+++ 07/Hilbert_inv_sum.py
@@ -2,7 +2,7 @@ import numpy
 import scipy
 format='%s%20.15f%20.15f%20.15f'
 
-N = 5
+N = 20
 
 for i in range(N):
     nmax = i+1
@@ -22,11 +22,11 @@ for i in range(N):
 
     A_inv = numpy.linalg.solve(A, numpy.eye(nmax))
 
-    print(A_inv, '\n')
+    #print(A_inv, '\n')
 
     #zkouška
     result = A @ A_inv
     print(result)
 
     sum_A_inv = numpy.sum(A_inv)
-    print("Suma všch prvků inverzní matice:", sum_A_inv)
+    print("Suma všech prvků inverzní matice:", sum_A_inv)
